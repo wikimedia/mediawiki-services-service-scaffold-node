9596ca88440338013e28813ed4b45306d2b2f722
daniel <dkinzler@wikimedia.org>
2022-02-18 17:03:38 +0100

    Add documentation for the root routes.
    Change-Id: I979f25a0e5c152318611861e77569010b4fa900b

--- routes/root.js
+++ routes/root.js
@@ -17,8 +17,18 @@ module.exports = (appObj) => {
 
     app = appObj;
 
-    router.get('/', routes.getSpecRouteHandler( app.conf.spec ) );
+    // Expose the swagger UI self-documentation at "/".
+    // The swagger UI will only be sent if the client accepts a text/html
+    // response, or the "doc" parameter is sent. You can set the second
+    // parameter (asDefault) to false in order to only expose the documentation
+    // at "/?doc", so other content can be served from plain "/".
     router.get('/', routes.getSwaggerUiRouteHandler( app ) );
+
+    // Expose the swagger spec at "/?spec"
+    router.get('/', routes.getSpecRouteHandler( app.conf.spec ) );
+
+    // Expose service health at "/healthz".
+    // See <https://kubernetes.io/docs/reference/using-api/health-checks/>
     router.get('/healthz', routes.getHealthZRouteHandler( app, () => 'pass' ) );
 
     return {
